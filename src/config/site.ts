ab797aafaacd9bfe33ac41b24a44f9cdf67ceaa5
Omar Saad <omarisadev@gmail.com>
2025-05-27 14:16:18 +0300

    minimized footer code and added site config file

---
+++ src/config/site.ts
@@ -0,0 +1,8 @@
+export const siteConfig = {
+    socialLinks: {
+        discord: "https://discord.gg/wPDyfR5AV9",
+        github: "https://github.com/Exodia-Os",
+        reddit: "https://www.reddit.com/r/ExodiaOS",
+        telegram: "https://t.me/exodiaos"
+    }
+}
